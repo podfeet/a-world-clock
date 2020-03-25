ab2c8ef71c0506be34214bc7bf0ca4f807bb0383
NosillaCast <allison@podfeet.com>
2020-03-24 20:41:31 -0700

    rudimentary radio buttons

--- index.js
+++ index.js
@@ -15,3 +15,54 @@
 * What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
 * Errors thrown e.g. @throws {RangeError} and why
 */
+
+/**
+* First test of moment.js
+* 
+* What comes in e.g. @param {string} boogers which is the string
+* What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
+* Errors thrown e.g. @throws {RangeError} and why
+*/
+
+// showTime();
+function showTime() {
+  var now = moment();
+  alert(`It is ${now} at Allison's house`);
+}
+
+// ************** //
+// Click Handlers //
+// ************** //
+
+/**
+* Click Handler for Timezone selection (Select)
+*
+* What comes in e.g. @param {string} boogers which is the string
+* What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
+* Errors thrown e.g. @throws {RangeError} and why
+*/
+
+/**
+* Click Handler for 12 vs 24 hr selection (radio)
+*
+* What comes in e.g. @param {string} boogers which is the string
+* What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
+* Errors thrown e.g. @throws {RangeError} and why
+*/
+
+/**
+* Click Handler for show seconds option (radio)
+*
+* What comes in e.g. @param {string} boogers which is the string
+* What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
+* Errors thrown e.g. @throws {RangeError} and why
+*/
+
+/**
+* Click Handler for pulsing divider option (radio)
+* Can I animate the radio button to help illustrate?
+*
+* What comes in e.g. @param {string} boogers which is the string
+* What goes out e.g. @return {jQuery} Returns a jQuery ojbect that contains...
+* Errors thrown e.g. @throws {RangeError} and why
+*/
