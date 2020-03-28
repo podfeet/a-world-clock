f23d91a625221f7c4c8dc7befdeb535cc097d2e6
NosillaCast <allison@podfeet.com>
2020-03-28 14:35:36 -0700

    refactored renderTime - show/hide seconds is working

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ let TIME12WSEC = moment().format('h:mm:ss a');
 let TIME12WOSEC = moment().format('h:mm a');
 let TIME24WSEC = moment().format('HH:mm:ss');
 let TIME24WOSEC = moment().format('HH:mm:ss');
-let FORMATTEDTIME = ''; // time formatted as user requests
+let FORMATTEDTIME = TIME12WSEC; // Default formatted time
 
-let TIMEVIEW = ''; // the view for the time Mustache
-
-// let showTime = function; // a function to figure out which formatted time the user has requested
-
-
+let TIMEVIEW = {}; // the view for the time Mustache
 // 
 // Document Ready Handler
 // 
+$(function(){
+  console.log('starting DOM');
+  renderTime();
+  console.log('I am done with DOM');
+});
 
 // Document functions
 /**
@@ -35,12 +36,6 @@ let TIMEVIEW = ''; // the view for the time Mustache
 * Errors thrown e.g. @throws {RangeError} and why
 */
 
-// showTime();
-// function showTime() {
-//   var now = moment();
-//   alert(`The time is ${now}`);
-// }
-
 /**
 * Define Time format
 *
@@ -50,10 +45,18 @@ let TIMEVIEW = ''; // the view for the time Mustache
 * @return {string} Returns a string that defines the time format
 * Errors thrown e.g. @throws {RangeError} and why
 */
+TIMEVIEW = {
+  time: FORMATTEDTIME // default is 12 hour with seconds
+}
+function renderTime(){
+  $('#forTime').empty();
+  $('#forTime').html(Mustache.render($('#timeTpl').html(), TIMEVIEW));
+}
+// 
 
-// <!-- NEEDS A CLICK HANDLER -->
-// <!-- THEN NESTED 12 VS 24 HR -->
-function showTime(){
+
+// <!-- NEEDS NESTED 12 VS 24 HR -->
+$('#showSeconds').click(function(){
   if ($("input[id][name$='showSeconds']").prop( "checked" )) {
     // console.log('showSeconds is yes');
     console.log(`12 hour time with seconds is ${TIME12WSEC}`);
@@ -62,16 +65,16 @@ function showTime(){
     console.log(`12 hour time without seconds is ${TIME12WOSEC}`);
     FORMATTEDTIME = TIME12WOSEC;
   } 
-}
-showTime();
-
-TIMEVIEW = {
-  time: FORMATTEDTIME
-}
-// moment().format('MMMM Do YYYY, h:mm:ss a');
-$(function(){
-  $('#forTime').html(Mustache.render($('#timeTpl').html(), TIMEVIEW));
+  // had to repeate TIMEVIEW in here because it wasn't getting updated with new FORMATTEDTIME
+  TIMEVIEW = {
+    time: FORMATTEDTIME // should be showSeconds option
+  }
+  renderTime();
 })
+
+
+
+// moment().format('MMMM Do YYYY, h:mm:ss a');
 
 // function timeFunction(ss,nh){
 
