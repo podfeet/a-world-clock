642ba04501a3af6c7bae5c576786311329fdc1fb
NosillaCast <allison@podfeet.com>
2020-08-28 11:55:40 -0700

    fixed typo on false

--- bootstrap-4-autocomplete.js
+++ bootstrap-4-autocomplete.js
@@ -2,7 +2,7 @@
     var defaults = {
         treshold: 4,
         maximumItems: 5,
-        highlightTyped: falseœ,
+        highlightTyped: false,
         highlightClass: 'text-primary'
     };
     function createItem(lookup, item, opts) {
